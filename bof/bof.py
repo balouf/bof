99771e4533d4c54d1e0324661234b2e197eef3c0
fabien <fabien.mathieu@nokia-bell-labs.com>
2020-12-12 13:37:36 +0100

    Doc update

--- bof/bof.py
+++ bof/bof.py
@@ -3,36 +3,46 @@ def default_preprocessor(txt):
 
 
 class FactorTree:
-    def __init__(self, txt_list=None, auto_update=False, preprocessor=None, n_range=5):
+    def __init__(self, corpus=None, auto_update=False, preprocessor=None, n_range=5):
         """
         Maintain a Tree of factor of a given corpus.
 
         Parameters
         ----------
-        txt_list
+        corpus
         auto_update
         preprocessor
         n_range
 
         Attributes
         ----------
+        count
+        edges
+
+        Examples
+        --------
+
+        >>> corpus = ["riri", "fifi", "rififi"]
+        >>> tree = FactorTree(corpus=corpus)
+        >>> tree.factors
+        [8, 8, 15]
         """
-        self.nodes = [dict()]
+        self.count = [dict()]
         self.edges = [dict()]
-        self.txt_list = []
-        self.txt_dict = dict()
+        self.corpus_list = []
+        self.corpus_dict = dict()
         self.factor_list = [""]
         self.factor_dict = {"": 0}
         self.factors = []
-        self.n = 1
-        self.m = 0
+        self.m = 1
+        self.n = 0
         self.auto_update = auto_update
         if preprocessor is None:
             preprocessor = default_preprocessor
         self.preprocessor = preprocessor
         self.n_range = n_range
-        if txt_list is not None:
-            self.add_txt_list_to_tree(txt_list)
+        if corpus is not None:
+            self.add_txt_list_to_tree(corpus)
 
     def add_txt_list_to_tree(self, txt_list):
         for txt in txt_list:
@@ -43,60 +53,60 @@ class FactorTree:
         length = len(txt)
 
         # Empty factor my friend!
-        self.nodes[0][self.m] = len(txt) + 1
+        self.count[0][self.n] = len(txt) + 1
         self.factors.append(1)
 
         for start in range(length):
             node = 0
             end = min(start + self.n_range, length) if self.n_range else length
             for letter in txt[start:end]:
-                n_node = self.edges[node].setdefault(letter, self.n)
-                if n_node == self.n:
+                n_node = self.edges[node].setdefault(letter, self.m)
+                if n_node == self.m:
                     self.edges.append(dict())
-                    self.nodes.append(dict())
+                    self.count.append(dict())
                     fac = self.factor_list[node] + letter
                     self.factor_list.append(fac)
-                    self.factor_dict[fac] = self.n
-                    self.n += 1
+                    self.factor_dict[fac] = self.m
+                    self.m += 1
                 node = n_node
-                d = self.nodes[node]
-                if d.setdefault(self.m, 0) == 0:
-                    self.factors[self.m] += 1
-                d[self.m] += 1
-        self.txt_list.append(txt)
-        self.txt_dict[txt] = self.m
-        self.m += 1
+                d = self.count[node]
+                if d.setdefault(self.n, 0) == 0:
+                    self.factors[self.n] += 1
+                d[self.n] += 1
+        self.corpus_list.append(txt)
+        self.corpus_dict[txt] = self.n
+        self.n += 1
 
     def common_factors(self, txt):
         txt = self.preprocessor(txt)
-        index = self.txt_dict.get(txt)
+        index = self.corpus_dict.get(txt)
         if not index:
             if not self.auto_update:
                 return self.common_factors_external(txt)
-            index = self.m
+            index = self.n
             self.add_txt_to_tree(txt)
         return self.common_factors_internal(index)
 
     def common_factors_internal(self, i):
         buffer = {0}
-        res = [0] * self.m
+        res = [0] * self.n
         while buffer:
             node = buffer.pop()
-            if i in self.nodes[node]:
-                for j in self.nodes[node]:
+            if i in self.count[node]:
+                for j in self.count[node]:
                     res[j] += 1
                 buffer.update(self.edges[node].values())
         return res, self.factors[i]
 
     def common_factors_external(self, txt):
         buffer = {0}
-        res = [0] * self.m
+        res = [0] * self.n
         new_tree = FactorTree([txt], preprocessor=self.preprocessor, n_range=self.n_range)
         while buffer:
             node = buffer.pop()
             target = self.factor_dict.get(new_tree.factor_list[node])
             if target is not None:
-                for j in self.nodes[target]:
+                for j in self.count[target]:
                     res[j] += 1
                 buffer.update(new_tree.edges[node].values())
         return res, new_tree.factors[0]
