0c9c9c03eadf218e142d8eba05c0c923ddfcb184
fabien <fabien.mathieu@nokia-bell-labs.com>
2020-12-30 15:32:18 +0100

    Bump version: 0.3.1 → 0.3.2

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/balouf/bof',
-    version='0.3.1',
+    version='0.3.2',
     zip_safe=False,
     ext_modules=get_cython_extensions()
 )
