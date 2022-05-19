a6522054e5bbdf1d4dec9e225661c838ff172fe6
fabien <fabien.mathieu@nokia-bell-labs.com>
2022-05-19 19:31:18 +0200

    3.10 compatibility

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['dill', 'numba', 'numpy', 'scipy']
+requirements = ['dill', 'numba', 'numpy>=1.21.4', 'scipy']
 
 setup_requirements = ['pytest-runner', ]
 
