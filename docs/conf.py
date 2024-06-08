12d841fb20bf8a367a8183deb23c2146c6ba16fe
fabien <fabien.mathieu@nokia-bell-labs.com>
2024-06-08 08:41:18 +0200

    Prepare 0.4

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ import bof
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.napoleon', 'sphinx.ext.githubpages',
-              'sphinx.ext.intersphinx']
+              'sphinx.ext.intersphinx', 'sphinx_mdinclude']
 
 # Add the possibility to access python documentation.
 intersphinx_mapping = {'python':('https://docs.python.org/3', None),
